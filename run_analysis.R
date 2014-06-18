8c1fdc12e78f24110f57bbc813db57580e2238c9
Carlos P Montoya <cpmontoya@verizon.net>
2014-06-18 02:32:14 -0400

    R script - part1

--- run_analysis.R
+++ run_analysis.R
@@ -5,3 +5,17 @@ if(!file.exists("UCI Har Dataset")){
 } else {
      print("Data is already downloaded and unzipped")
 }
+testdata<-read.table("UCI HAR Dataset/test/x_test.txt")
+traindata<-read.table("UCI HAR Dataset/train/x_train.txt")
+features<-read.table("UCI HAR Dataset/features.txt")
+names(testdata)<-features[[2]]
+names(traindata)<-features[[2]]
+testsubjects<-read.table("UCI HAR Dataset/test/subject_test.txt")
+trainsubjects<-read.table("UCI HAR Dataset/train/subject_train.txt")
+testactive<-read.table("UCI HAR Dataset/test/y_test.txt")
+trainactive<-read.table("UCI HAR Dataset/train/y_train.txt")
+testdata<-cbind(testsubjects,testactive,testdata)
+traindata<-cbind(trainsubjects,trainactive,traindata)
+totaldata<-rbind(traindata,testdata)
+names(totaldata)[1]<-"subjects"
+names(totaldata)[2]<-"activity"
